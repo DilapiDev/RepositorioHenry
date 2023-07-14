1db20829843416253432863b74607bc429c70493
DilapiDev <giubertdilapi@gmail.com>
2023-07-13 22:49:01 -0300

    ejercicio 1 de js

---
+++ M03/ejercicio1.js
@@ -0,0 +1,39 @@
+/*1️⃣ EJERCICIO 01 1️⃣*/
+
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+const nuevoString = "Soy Giubert Di Lapi";
+
+// Crea una variable de tipo number.
+const nuevoNumero = 2806;
+
+// Crea una variable de tipo boolean.
+const nuevoBoolean = true;
+
+// Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - 7 === 3;
+
+// Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * 4 === 40;
+
+// Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === 1;
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   nuevoString,
+   nuevoNumero,
+   nuevoBoolean,
+   nuevaResta,
+   nuevaMultiplicacion,
+   nuevoModulo, 
+};
+
+console.log(nuevoString);
+console.log(nuevoNumero);
+console.log(nuevoBoolean);
+console.log(nuevaResta);
+console.log(nuevaMultiplicacion);
+console.log(nuevoModulo)
